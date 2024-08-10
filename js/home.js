c696187349c49e3161e864bd8814151095bc097d
Allam-Hicham <146976469+Allam-Hicham@users.noreply.github.com>
2024-08-10 09:36:25 +0100

    Update home.js
    add suffix and other stuff and target self

--- js/home.js
+++ js/home.js
@@ -11,7 +11,7 @@ function shuffleArray(array) {
   return array;
 }
 
-mainUrl = 'AKfycbxeIbHXClbj2Zz1Nd9smtBKfpWxFfEBbwDRpZhBPJ3hJktby8ky6oAwPSo0FEI9e51d';
+mainUrl = 'AKfycbwfAWA6P_foZey8vmlGgt1qecP8Oh1aeTqfF3ZY35JEzsYZ6g9Kekt8aD7JMg-0HvUC';
 var searchUrl;
 var sendSearchUrl;
 async function templateCards() {
@@ -22,32 +22,32 @@ async function templateCards() {
    //gallery
     document.querySelector('.container').innerHTML = `
         <div class="mySlides">/
-            <a onclick="getAnimeCard('${data[0].animeName}', '${data[0].rate}', '${data[0].statut}', '${data[0].imageUrl}', '${data[0].hash}')">
+            <a onclick="getAnimeCard('${data[0].animeName}', '${data[0].rate}', '${data[0].statut}', '${data[0].imageUrl}', '${data[0].hash}','${data[0].suffix}', '${data[0].animeJp}', '${data[0].animeAr}')">
                 <img src="${data[0].state}"  style="width: 100%;">
             </a>
         </div>
         <div class="mySlides">/
-            <a onclick="getAnimeCard('${data[1].animeName}', '${data[1].rate}', '${data[1].statut}', '${data[1].imageUrl}', '${data[1].hash}')">
+            <a onclick="getAnimeCard('${data[1].animeName}', '${data[1].rate}', '${data[1].statut}', '${data[1].imageUrl}', '${data[1].hash}', '${data[1].suffix}', '${data[1].animeJp}', '${data[1].animeAr}')">
                 <img src="${data[1].state}"  style="width: 100%;">
             </a>
         </div>
         <div class="mySlides">/
-            <a onclick="getAnimeCard('${data[2].animeName}', '${data[2].rate}', '${data[2].statut}', '${data[2].imageUrl}', '${data[2].hash}')">
+            <a onclick="getAnimeCard('${data[2].animeName}', '${data[2].rate}', '${data[2].statut}', '${data[2].imageUrl}', '${data[2].hash}', '${data[2].suffix}', '${data[2].animeJp}', '${data[2].animeAr}')">
                 <img src="${data[2].state}"  style="width: 100%;">
             </a>
         </div>
         <div class="mySlides">/
-            <a onclick="getAnimeCard('${data[3].animeName}', '${data[3].rate}', '${data[3].statut}', '${data[3].imageUrl}', '${data[3].hash}')">
+            <a onclick="getAnimeCard('${data[3].animeName}', '${data[3].rate}', '${data[3].statut}', '${data[3].imageUrl}', '${data[3].hash}', '${data[3].suffix}', '${data[3].animeJp}', '${data[3].animeAr}')">
                 <img src="${data[3].state}"  style="width: 100%;">
             </a>
         </div>
         <div class="mySlides">/
-            <a onclick="getAnimeCard('${data[4].animeName}', '${data[4].rate}', '${data[4].statut}', '${data[4].imageUrl}', '${data[4].hash}')">
+            <a onclick="getAnimeCard('${data[4].animeName}', '${data[4].rate}', '${data[4].statut}', '${data[4].imageUrl}', '${data[4].hash}', '${data[4].suffix}', '${data[4].animeJp}', '${data[4].animeAr}')">
                 <img src="${data[4].state}"  style="width: 100%;">
             </a>
         </div>
         <div class="mySlides">/
-            <a onclick="getAnimeCard('${data[5].animeName}', '${data[5].rate}', '${data[5].statut}', '${data[5].imageUrl}', '${data[5].hash}')">
+            <a onclick="getAnimeCard('${data[5].animeName}', '${data[5].rate}', '${data[5].statut}', '${data[5].imageUrl}', '${data[5].hash}', '${data[5].suffix}', '${data[5].animeJp}', '${data[5].animeAr}')">
                 <img src="${data[5].state}"  style="width: 100%;">
             </a>
         </div>
@@ -74,7 +74,7 @@ sendSearchUrl = data[7].rate;
     data = data.filter(item => item.animeType && item.animeType.trim() !== '');
     const shData = shuffleArray(data);
     document.getElementById('anime_suggestion_grid').innerHTML = shData.map((item,Id) => `
-      <a class="anime_card" style="background-image: url(${item.imageUrl});" onclick="getAnimeCard('${item.animeName}', '${item.rate}', '${item.statut}', '${item.imageUrl}', '${item.hash}')">&nbsp;${item.state}
+      <a class="anime_card" style="background-image: url(${item.imageUrl});" onclick="getAnimeCard('${item.animeName}', '${item.rate}', '${item.statut}', '${item.imageUrl}', '${item.hash}', '${item.suffix}', '${item.animeJp}', '${item.animeAr}')">&nbsp;${item.state}
         <p class="card-type">&nbsp;${item.animeType}</p>
         <p class="card-title">${item.animeName}</p>
         <p class="card-rate">${item.rate}⭐ | ${item.statut} </p>
@@ -90,11 +90,11 @@ sendSearchUrl = data[7].rate;
 }
 
 // Get anime description
-async function getAnimeCard(animeName, rate, statu, imageUrl, Hash) {
-    localStorage.setItem('animeEntry0',JSON.stringify([rate,statu,imageUrl]));
+async function getAnimeCard(animeName, rate, statu, imageUrl, Hash, suffix, animeJp, animeAr) {
+    localStorage.setItem('animeEntry0',JSON.stringify([rate,statu,imageUrl,suffix,animeJp,animeAr]));
     document.getElementById('spinner_container').style.display = 'block';
     document.getElementById('description').style.display = 'block';
-    console.log(animeName,rate,statu,imageUrl,Hash);
+    console.log(animeName,rate,statu,imageUrl,Hash,suffix,animeJp,animeAr);
     try {
       const response = await fetch(`https://script.google.com/macros/s/${Hash}/exec?term=${encodeURIComponent(animeName)}`);   
       if (!response.ok) {
@@ -136,7 +136,7 @@ async function getAnimeCard(animeName, rate, statu, imageUrl, Hash) {
         document.getElementById('phone-container').innerHTML = `
             <div class="description_right">
                 <p style="text-align:center;background-color:rgba(16,0, 16, 0.3);"> اضغط على الزر أسفله 👇 لمشاهدة الأنمي</p>
-                <a id="watch-now" href="watch.html" target="_blank"><b>شاهد الآن</b></a>
+                <a id="watch-now" href="watch.html" target="_self"><b>شاهد الآن</b></a>
             </div> `;
         document.getElementById('phone-container').style.padding = '0 0 10px 0';
       } else {
