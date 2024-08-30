a7853107796e7edab2c9067e049eb74aa8a29816
Allam-Hicham <146976469+Allam-Hicham@users.noreply.github.com>
2024-08-30 23:27:59 +0100

    Update home.js

--- js/home.js
+++ js/home.js
@@ -197,11 +197,9 @@ async function saveUserData() {
       let pageStats = 'N';//'music-tik';
       let animeLost = localStorage.getItem('animeLost');//'one piece-s1';
       let watchLost = localStorage.getItem('watchLost');//'solo leveling s1-12-45-899-4';
-      fetch('https://api.ipify.org/?format=json')
-      .then(response => response.json())
-      .then(data => {
-      const ip = data.ip;
-      });
+      const response_ip = await fetch('https://api.ipify.org/?format=json');
+      const data_ip = await response_ip.json();
+      const ip = await data_ip.ip; 
       console.log(ip);
       const userData = { userID, username, password, email, pageStats, animeLost, watchLost, ip };
       console.log(userData);
